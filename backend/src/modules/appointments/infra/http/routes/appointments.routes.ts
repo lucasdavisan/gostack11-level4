d4d711b6d680547640301483277ac7add5ee489f
Lucas Davi <lucasdavisan@gmail.com>
2020-08-06 21:00:11 -0300

    Dependency Inversion Principle.

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
@@ -8,6 +7,7 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 /** Route: should always be concerned with receiving the requisiton,
  *  call another file and return a response. Otherwise we must be
@@ -15,19 +15,20 @@ const appointmentsRouter = Router();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-	const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-	const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async (request, response) => {
+// 	const appointments = await appointmentsRepository.find();
 
-	return response.json(appointments);
-});
+// 	return response.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
 	const { provider_id, date } = request.body;
 
 	const parsedDate = parseISO(date);
 
-	const createAppointment = new CreateAppointmentService();
+	const createAppointment = new CreateAppointmentService(
+		appointmentsRepository,
+	);
 
 	const appointment = await createAppointment.execute({
 		date: parsedDate,
